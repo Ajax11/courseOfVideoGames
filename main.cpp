a49d599981b2983bfdec744ae561ba531eb56314
Ajax11 <alejandro.delgado@ucsp.edu.pe>
2018-09-05 18:38:25 -0500

    adding main file
    Signed-off-by: Ajax11 <alejandro.delgado@ucsp.edu.pe>

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>     // std::cout
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include "grid.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	
+	grid table;
+
+	return 0;
+}
